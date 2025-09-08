a3a9ceb8eae16363c5b020a1be5aa5a3caa22905
blackpirateapps <github@blackpiratex.com>
2025-09-08 09:37:55 +0530

    tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./public/**/*.html", // Scans all .html files in the public folder and subfolders
-    "./public/**/*.js"// Scans all .js files in the public folder and subfolders
+    "./public/**/*.js", // Scans all .js files in the public folder and subfolders
     "./public/*.html" 
   ],
   theme: {
